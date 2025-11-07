5c1e217947f0fdf6acd39cce56aad4c248152e1f
chadcode88 <heymrcurious@gmail.com>
2025-11-07 22:03:07 +0530

    first commit

---
+++ jspractice.js
@@ -0,0 +1,19 @@
+let bill = 5;
+
+
+// console.log(bill)
+function isBillEqual(){
+    if (bill == 5) {
+        console.log(bill -= 1)
+        return bill += 1
+    }
+    else{
+        return ;
+    }
+}
+//javascript default behaviur is responsible for the return statment undfined value in the console
+
+console.log(isBillEqual());
+let cart;
+cart = 4
+console.log(cart)
